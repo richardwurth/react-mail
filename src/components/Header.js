94aafd659771e83195d48c4d7f0a1de1cbb863ec
Richard Wurth <Richard Wurth>
2017-04-15 13:33:44 -0600

    Initial submission of React Router - Day One

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,33 @@
 import React from "react";
+import {Link} from 'react-router';
+import './hover.css';
 
 export default class Header extends React.Component {
 	render() {
 		const styles = this.getStyles();
 
 		return (
-			<header style={ styles.wrapper }>
-				<div>
-					<img
-						alt="React Logo"
-						src="assets/react-logo.svg"
-						style={ styles.logoImage }
-					/>
-					<h1 style={ styles.header }>rMail</h1>
-				</div>
+			<header className="wrapper header" style={ styles.wrapper }>
+					<div>
+						<img
+							alt="React Logo"
+							src="assets/react-logo.svg"
+							style={ styles.logoImage }
+						/>
+						<Link to="/"><h1 style={ styles.header }>rMail</h1></Link>
+					</div>
 
-				<div>
-				</div>
+					<div>
+						<Link to="/inbox">
+							<span className="link">Inbox</span>
+						</Link>
+						<Link to="/draft">
+							<span className="link">Draft</span>
+						</Link>
+						<Link to="/contacts">
+							<span className="link">Contacts</span>
+						</Link>
+					</div>
 			</header>
 		);
 	}
@@ -26,24 +37,26 @@ export default class Header extends React.Component {
 			header: {
 				color: "white"
 				, display: "inline-block"
+				, textDecoration: "none"
 			}
-			, link: {
-				color: "white"
-				, fontSize: 20
-				, marginRight: 7
-			}
+			// , link: {
+			// 	color: "white"
+			// 	, fontSize: 20
+			// 	, marginRight: 7
+			// }
 			, logoImage: {
 				height: 50
 				, margin: "0 10px 20px 10px"
 				, width: 50
-			}
-			, wrapper: {
-				alignItems: "baseline"
-				, backgroundColor: "black"
-				, boxSizing: "border-box"
-				, color: "white"
-				, display: "flex"
-				, justifyContent: "space-between"
+			// }
+			// , wrapper: {
+			// 	alignItems: "baseline"
+			// 	, backgroundColor: "black"
+			// 	, boxSizing: "border-box"
+			// 	, color: "white"
+			// 	, display: "flex"
+			// 	, textDecoration: "none"
+				// , justifyContent: "space-between"
 			}
 		}
 	}
--- src/components/MessageLink.js
+++ src/components/MessageLink.js
@@ -1,30 +1,46 @@
 import React from "react";
+import './hover.css';
 
 export default class MessageLink extends React.Component {
+
+
 	render() {
+
 		const styles= this.getStyles();
 
 		return (
-			<div style={ styles.wrapper }>
-				<h3>{ /* message name */ }</h3>
+			<div style={ styles.wrapper } className="wrapper">
+				<h3>{ this.props.name }</h3>
 
-				{ /* message email */ }
+				{ this.props.email }
 			</div>
 		);
 	}
+
+
+
+
+	// 	changeStyle() {
+	// 		return {
+	// 		  test : {
+	// 			fontSize : 28
+	// 		}
+	// 	}
+	// }
 
 	getStyles() {
 		return {
 			wrapper: {
 				alignItems: "baseline"
 				, border: "1px solid #e0e0e0"
-				, borderRadius: 4
+				, borderRadius: 8
 				, display: "flex"
 				, justifyContent: "space-between"
 				, margin: 10
 				, padding: 10
 				, width: "95%"
+				, transition: "all .3s"
+			}
 			}
 		}
 	}
-}
